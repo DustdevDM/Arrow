e7a71a41cb8e534a3733e82d2862398bd7bf4dc4
DustinDEV2more <public@dustin-dm.de>
2023-04-09 22:19:53 +0200

    add bootup functions

---
+++ EnvLoader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace DND_DC_Music_Bot
+{
+    public class EnvLoader
+    {
+        public static void Load(string filePath)
+        {
+            // src: https://dusted.codes/dotenv-in-dotnet
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException("Unable to find .env file");
+
+            foreach (var line in File.ReadAllLines(filePath))
+            {
+                var parts = line.Split(
+                    '=',
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length != 2)
+                    continue;
+
+                Environment.SetEnvironmentVariable(parts[0], parts[1]);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+using DND_DC_Music_Bot;
+using System.Diagnostics;
+
+public class Program
+{
+    private DiscordSocketClient _client;
+
+    public static Task Main(string[] args) => new Program().MainAsync(args);
+
+    public async Task MainAsync(string[] args)
+    {
+        if (args.Count() == 0) throw new ArgumentNullException("Missing .env Path Argument");
+
+        EnvLoader.Load(args[0]);
+
+        _client = new DiscordSocketClient();
+
+        _client.Log += Log;
+
+        var token = Environment.GetEnvironmentVariable("DISCORDTOKEN");
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
