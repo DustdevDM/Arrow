352cb70f577d0305622ee7d0af60e28c6e4a7438
DustinDEV2more <public@dustin-dm.de>
2023-05-10 20:41:45 +0200

    add dependecy injection

---
+++ DependecyInjectionModule.cs
@@ -0,0 +1,14 @@
+﻿using Discord.WebSocket;
+using Ninject;
+using Ninject.Modules;
+
+namespace DND_DC_Music_Bot
+{
+    internal class DependecyInjectionModule : NinjectModule
+    {
+        public override void Load()
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,28 +1,35 @@
 ﻿using Discord;
 using Discord.WebSocket;
 using DND_DC_Music_Bot;
+using Ninject;
 using System.Diagnostics;
 
 public class Program
 {
-    private DiscordSocketClient _client;
+    private DiscordSocketClient? client;
 
     public static Task Main(string[] args) => new Program().MainAsync(args);
 
     public async Task MainAsync(string[] args)
     {
+        //Initialize Ninject Dependecy Injection
+        var kernel = new StandardKernel();
+        client = kernel.Get<DiscordSocketClient>();
+
+        //Check for .env file
         if (args.Count() == 0) throw new ArgumentNullException("Missing .env Path Argument");
 
+        //Load .env file
         EnvLoader.Load(args[0]);
 
-        _client = new DiscordSocketClient();
 
-        _client.Log += Log;
+        client.Log += Log;
 
         var token = Environment.GetEnvironmentVariable("DISCORDTOKEN");
 
-        await _client.LoginAsync(TokenType.Bot, token);
-        await _client.StartAsync();
+        //Login and connect to Discord.
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
 
         // Block this task until the program is closed.
         await Task.Delay(-1);
