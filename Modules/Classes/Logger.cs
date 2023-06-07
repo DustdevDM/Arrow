6dcf20f2ce2223f6af86210eabbf1037cbe3e5da
DustinDEV2more <public@dustin-dm.de>
2023-06-07 21:39:47 +0200

    Add Logger Class

---
+++ Modules/Classes/Logger.cs
@@ -0,0 +1,17 @@
+﻿using Lavalink4NET.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DND_DC_Music_Bot.Modules.Classes
+{
+    public class Logger : ILogger
+    {
+        public void Log(object source, string message, LogLevel level = LogLevel.Information, Exception? exception = null)
+        {
+            Console.WriteLine($"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()} [{source.ToString()}] {message}");
+        }
+    }
+}
