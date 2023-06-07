c65ee4b117211bd8b01c025941bbf3f8c79ed092
DustinDEV2more <public@dustin-dm.de>
2023-06-07 21:39:33 +0200

    add Lavalink DependecyInjectionModules

--- DependecyInjectionModule.cs
+++ DependecyInjectionModule.cs
@@ -1,5 +1,12 @@
-﻿using DND_DC_Music_Bot.Modules.Services;
+﻿using Discord.WebSocket;
+using Discord;
+using DND_DC_Music_Bot.Modules.Services;
+using Lavalink4NET;
+using Lavalink4NET.DiscordNet;
+using Lavalink4NET.Logging;
 using Ninject.Modules;
+using DND_DC_Music_Bot.Modules.Classes;
+using DND_DC_Music_Bot.Modules.Classes.Commands;
 
 namespace DND_DC_Music_Bot
 {
@@ -15,6 +22,15 @@ namespace DND_DC_Music_Bot
         {
             this.Bind<ConfigService>().ToSelf().InSingletonScope();
             this.Bind<SlashCommandService>().ToSelf();
+            this.Bind<ILogger>().To<Logger>();
+
+            this.Bind<DiscordSocketClient>().ToSelf().InSingletonScope().WithConstructorArgument(new DiscordSocketConfig() { UseInteractionSnowflakeDate = false, GatewayIntents = GatewayIntents.GuildVoiceStates });
+
+            this.Bind<AddTrackCommand>().ToSelf();
+
+            this.Bind<IDiscordClientWrapper>().To<DiscordClientWrapper>().InSingletonScope();
+            ILavalinkCache cache = null;
+            this.Bind<IAudioService>().To<LavalinkNode>().InSingletonScope().WithConstructorArgument("options", new LavalinkNodeOptions() { RestUri = "fdskfjdkls", WebSocketUri = "fdsfdsf", Password = "fsdfdsf" }).WithConstructorArgument("cache", cache);
         }
     }
 }
