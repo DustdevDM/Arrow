a8279ada3fb4ad617c13e800d906bfa76bf32eca
DustinDEV2more <public@dustin-dm.de>
2023-06-07 21:40:24 +0200

    edit slahscommands

--- Modules/Classes/AddTrack.command.cs
+++ Modules/Classes/AddTrack.command.cs
@@ -1,24 +1,44 @@
 ﻿using Discord;
 using Discord.WebSocket;
 using DND_DC_Music_Bot.Modules.Interfaces;
+using Lavalink4NET;
+using Lavalink4NET.Player;
+using Lavalink4NET.Rest;
 
 namespace DND_DC_Music_Bot.Modules.Classes.Commands
 {
-    internal class AddTrackCommand : ISlashCommand
+    public class AddTrackCommand : ISlashCommand
     {
+        private IAudioService audioService;
+
+        public AddTrackCommand(IAudioService audioService)
+        {
+            this.audioService = audioService;
+        }
+
         public string Name => "track";
 
         public string Description => "Manage your tracks";
 
         public SlashCommandOptionBuilder[] Options => new SlashCommandOptionBuilder[]
         {
-            new SlashCommandOptionBuilder().WithName("add").WithDescription("Add a Track").WithType(ApplicationCommandOptionType.SubCommand),
+            new SlashCommandOptionBuilder().WithName("add").WithDescription("Add a Track").WithType(ApplicationCommandOptionType.SubCommand)
+                .AddOption("serach_query", ApplicationCommandOptionType.String, "The Search Query used to find your Song. Can be a Uri or a Song- or Videoname", true),
             new SlashCommandOptionBuilder().WithName("remove").WithDescription("Remove a Track").WithType(ApplicationCommandOptionType.SubCommand),
         };
 
         public Task Execute(SocketSlashCommand socketSlashCommand)
         {
-            socketSlashCommand.FollowupAsync("You used the " + socketSlashCommand.Data.Options.First().Name + " option");
+            switch (socketSlashCommand.Data.Options.First().Name)
+            {
+                case "add":
+                    this.AddTrackAsync(socketSlashCommand);
+                    break;
+                case "remove":
+                    this.RemoveTrack(socketSlashCommand);
+                    break;
+            }
+
             return Task.CompletedTask;
         }
 
@@ -26,5 +46,28 @@ namespace DND_DC_Music_Bot.Modules.Classes.Commands
         {
             return Task.FromResult(true);
         }
+
+        private async Task AddTrackAsync(SocketSlashCommand socketSlashCommand)
+        {
+            //get the search query
+            string searchQuery = socketSlashCommand.Data.Options.First().Options.First().Value.ToString();
+
+            LavalinkTrack myTrack = await this.audioService.GetTrackAsync(searchQuery, SearchMode.YouTube);
+
+            if (myTrack == null)
+            {
+                await socketSlashCommand.FollowupAsync("Track not found");
+                return;
+            }
+
+            await socketSlashCommand.FollowupAsync("Track added: " + myTrack.Uri);
+
+        }
+
+        private void RemoveTrack(SocketSlashCommand socketSlashCommand)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
--- Modules/Services/SlashcommandService.cs
+++ Modules/Services/SlashcommandService.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Reflection;
 using Discord;
 using Discord.Net;
 using Discord.WebSocket;
+using DND_DC_Music_Bot.Modules.Classes;
+using DND_DC_Music_Bot.Modules.Classes.Commands;
 using DND_DC_Music_Bot.Modules.Interfaces;
 using Newtonsoft.Json;
 
@@ -13,12 +16,21 @@ namespace DND_DC_Music_Bot.Modules.Services
     /// </summary>
     public class SlashCommandService
     {
+        private Logger logger;
+        private AddTrackCommand trackCommand;
+
+        public SlashCommandService(Logger logger, AddTrackCommand trackCommand)
+        {
+            this.logger = logger;
+            this.trackCommand = trackCommand;
+        }
+
         /// <summary>
         /// Handles any triggered Slashcommand.
         /// </summary>
         /// <param name="socketSlashCommand">Instance of <see cref="SocketSlashCommand"/></param>
         /// <returns></returns>
-        public static async Task HandleSlashCommand(SocketSlashCommand socketSlashCommand)
+        public async Task HandleSlashCommand(SocketSlashCommand socketSlashCommand)
         {
             await socketSlashCommand.DeferAsync();
 
@@ -27,79 +39,56 @@ namespace DND_DC_Music_Bot.Modules.Services
             IEnumerable<Type> slashCommands = Assembly.GetExecutingAssembly().GetTypes()
                                             .Where(t => t.Namespace == nspace && t.GetInterfaces().Contains(typeof(ISlashCommand)));
 
-            Type? slashCommand = slashCommands.FirstOrDefault(x =>
-            {
-                ISlashCommand? slashCommandInstance = Activator.CreateInstance(x) as ISlashCommand;
-                if (slashCommandInstance?.Name == socketSlashCommand.Data.Name)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            });
+            ISlashCommand slashCommandInstance = null;
 
-            if (slashCommand == null)
+            switch (socketSlashCommand.Data.Name)
             {
-                await socketSlashCommand.FollowupAsync("Failed to find Slashcommand");
-                return;
-            }
-
-            ISlashCommand? slashCommandInstance = Activator.CreateInstance(slashCommand) as ISlashCommand;
-            if (slashCommandInstance == null)
-            {
-                await socketSlashCommand.FollowupAsync("Failed to create Instance of Slashcommand");
-                return;
+                case "track":
+                    slashCommandInstance = this.trackCommand;
+                    break;
+                default:
+                    socketSlashCommand.RespondAsync("Command not found");
+                    return;
             }
 
             try
             {
-                Console.WriteLine($"[{nameof(SlashCommandService)}] Start execution of \"{slashCommandInstance.Name}\" which was ordered by user \"{socketSlashCommand.User.Username}\"({socketSlashCommand.User.Id}) on the {socketSlashCommand.ChannelId} channel.");
+                this.logger.Log(nameof(SlashCommandService), $"Start execution of \"{slashCommandInstance.Name}\" which was ordered by user \"{socketSlashCommand.User.Username}\"({socketSlashCommand.User.Id}) on the {socketSlashCommand.ChannelId} channel.");
                 bool validationResukt = await slashCommandInstance.Validate(socketSlashCommand);
                 if (validationResukt)
                 {
                 await slashCommandInstance.Execute(socketSlashCommand);
-                Console.WriteLine($"[{nameof(SlashCommandService)}] End execution of \"{slashCommandInstance.Name}\" which was ordered by user \"{socketSlashCommand.User.Username}\"({socketSlashCommand.User.Id}) on the {socketSlashCommand.ChannelId} channel.");
+                this.logger.Log(nameof(SlashCommandService), $"End execution of \"{slashCommandInstance.Name}\" which was ordered by user \"{socketSlashCommand.User.Username}\"({socketSlashCommand.User.Id}) on the {socketSlashCommand.ChannelId} channel.");
                 }
                 else
                 {
-                    Console.WriteLine($"[{nameof(SlashCommandService)}] Cancel execution due to negative validation result of \"{slashCommandInstance.Name}\" which was ordered by user \"{socketSlashCommand.User.Username}\"({socketSlashCommand.User.Id}) on the \"{socketSlashCommand.Channel.Name}\"({socketSlashCommand.ChannelId}) channel.");
+                    this.logger.Log(nameof(SlashCommandService), $"Cancel execution due to negative validation result of \"{slashCommandInstance.Name}\" which was ordered by user \"{socketSlashCommand.User.Username}\"({socketSlashCommand.User.Id}) on the \"{socketSlashCommand.Channel.Name}\"({socketSlashCommand.ChannelId}) channel.");
                 }
             }
-            catch (Exception exception)
+            catch(Exception e)
             {
-                Console.WriteLine($"[{nameof(SlashCommandService)}] Cought Exception while executing of \"{slashCommandInstance.Name}\" which was ordered by user \"{socketSlashCommand.User.Username}\"({socketSlashCommand.User.Id}) on the \"{socketSlashCommand.Channel.Name}\"({socketSlashCommand.ChannelId}) channel.\n{exception.Message}");
-                await socketSlashCommand.FollowupAsync(exception.Message);
+                this.logger.Log(nameof(SlashCommandService), $"Cought Exception while executing of \"{slashCommandInstance.Name}\" which was ordered by user \"{socketSlashCommand.User.Username}\"({socketSlashCommand.User.Id}) on the {socketSlashCommand.ChannelId} channel.\n{e}");
+                await socketSlashCommand.FollowupAsync(e.Message ?? "Something went wrong");
             }
         }
 
         /// <summary>
         /// Import and Register all Slashcommands that are within the ".Modules.Classes.Commands" Namespace.
         /// </summary>
-        public static async Task ImportAndRegisterCommands(DiscordSocketClient discordSocketClient)
+        public async Task ImportAndRegisterCommands(DiscordSocketClient discordSocketClient)
         {
-            string nspace = "DND_DC_Music_Bot.Modules.Classes.Commands";
+            List<ISlashCommand> slashCommands = new List<ISlashCommand>();
 
-            IEnumerable<Type> slashCommands = Assembly.GetExecutingAssembly().GetTypes()
-                                .Where(t => t.Namespace == nspace && t.GetInterfaces().Contains(typeof(ISlashCommand)));
+            slashCommands.Add(this.trackCommand);
 
-            foreach (Type slashCommand in slashCommands)
+            foreach (ISlashCommand slashCommand in slashCommands)
             {
-                ISlashCommand? slashCommandInstance = Activator.CreateInstance(slashCommand) as ISlashCommand;
-
-                if (slashCommandInstance == null)
-                {
-                    Console.WriteLine($"[{nameof(SlashCommandService)}] Failed to create Instance of Slashcommand");
-                    continue;
-                }
-
                 SlashCommandBuilder slashCommandAPIInstance = new();
-                slashCommandAPIInstance.WithName(slashCommandInstance.Name);
-                slashCommandAPIInstance.WithDescription(slashCommandInstance.Description);
-                slashCommandAPIInstance.AddOptions(slashCommandInstance.Options);
+                slashCommandAPIInstance.WithName(slashCommand.Name);
+                slashCommandAPIInstance.WithDescription(slashCommand.Description);
+                slashCommandAPIInstance.AddOptions(slashCommand.Options);
 
-                await PushCommandtoAPI(discordSocketClient, slashCommandAPIInstance);
+                await this.PushCommandtoAPI(discordSocketClient, slashCommandAPIInstance);
             }
 
             return;
@@ -108,7 +97,7 @@ namespace DND_DC_Music_Bot.Modules.Services
         /// <summary>
         /// Clear all Slashcommands from the Discord API.
         /// </summary>
-        public static async Task ClearCommands(DiscordSocketClient discordSocketClient)
+        public async Task ClearCommands(DiscordSocketClient discordSocketClient)
         {
             IReadOnlyCollection<SocketApplicationCommand> socketApplicationCommands = await discordSocketClient.GetGlobalApplicationCommandsAsync();
 
@@ -116,30 +105,30 @@ namespace DND_DC_Music_Bot.Modules.Services
             {
                 try
                 {
-                    Console.WriteLine($"[{nameof(SlashCommandService)}] Removing \"{socketApplicationCommand.Name}\" from Discord API");
+                    this.logger.Log(nameof(SlashCommandService), $"Removing \"{socketApplicationCommand.Name}\" from Discord API");
                     await socketApplicationCommand.DeleteAsync();
                     return;
                 }
                 catch (HttpException exception)
                 {
                     var json = JsonConvert.SerializeObject(exception.Errors.First(), Formatting.Indented);
-                    Console.WriteLine($"[{nameof(SlashCommandService)}] Caught Exception while removing SlashCommand from Discord:\n{json}");
+                    this.logger.Log(nameof(SlashCommandService), $"Caught Exception while removing SlashCommand from Discord:\n{json}");
                 }
             }
         }
 
-        private static async Task PushCommandtoAPI(DiscordSocketClient discordSocketClient, SlashCommandBuilder slashCommand)
+        private async Task PushCommandtoAPI(DiscordSocketClient discordSocketClient, SlashCommandBuilder slashCommand)
         {
             try
             {
-                Console.WriteLine($"[{nameof(SlashCommandService)}] Pushing \"{slashCommand.Name}\" to Discord API");
+                this.logger.Log(nameof(SlashCommandService), $"Pushing \"{slashCommand.Name}\" to Discord API");
                 await discordSocketClient.CreateGlobalApplicationCommandAsync(slashCommand.Build());
                 return;
             }
             catch (HttpException exception)
             {
                 var json = JsonConvert.SerializeObject(exception.Errors.First(), Formatting.Indented);
-                Console.WriteLine($"[{nameof(SlashCommandService)}] Caught Exception while pushing SlashCommand to Discord:\n{json}");
+                this.logger.Log(nameof(SlashCommandService), $"Caught Exception while pushing SlashCommand to Discord:\n{json}");
             }
         }
     }
